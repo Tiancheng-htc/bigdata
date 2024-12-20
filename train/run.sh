5794a2889a25da3a4babbfce2ece79d2a789da76
hutiancheng <tianchenghu.21@gmail.com>
2024-12-20 11:42:17 +0000

    dp run

--- train/run.sh
+++ train/run.sh
@@ -2,4 +2,4 @@ slaver="b@192.168.123.47"
 
 ./master.sh &
 
-ssh $slaver "cd /home/bigdata/train && ./slaver.sh"
+ssh $slaver "source ~/miniconda3/etc/profile.d/conda.sh && conda activate vllm && cd /home/b/bigdata/train && ./slaver.sh"
